01809bbf14d9cd9e5e0428108f3d307b17fcefc5
Gorileng <keneilwe963@gmail.com>
2024-02-27 16:38:08 -0800

    updated

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
-/* the eslint-disable import/extensions */
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
+/**
+ * Creates an array of {@link ClassRoom}s with a specific size.
+ * @returns An array of {@link ClassRoom}s.
+ */
 export default function initializeRooms() {
-  const room1 = new ClassRoom(19);
-  const room2 = new ClassRoom(20);
-  const room3 = new ClassRoom(34);
-  return [room1, room2, room3];
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
