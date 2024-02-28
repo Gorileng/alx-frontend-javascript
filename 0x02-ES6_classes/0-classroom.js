29d0dc039610a113967272b313e6e80b612831b3
Gorileng <keneilwe963@gmail.com>
2024-02-27 16:32:53 -0800

    updated

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,12 @@
-/* the eslint-disable */
+/**
+ * Represents a class room.
+ */
 export default class ClassRoom {
-	constructor(maxStudentSize) {
-		this._maxStudentSize = maxStudentSize;
-	}
-}
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
